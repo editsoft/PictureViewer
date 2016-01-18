c9a9c8b7486e6745e2aa576441d1bd6f23b04c3a
editsoft <jeevan.editsoft@gmail.com>
2016-01-18 11:56:20 +0530

    mycommit2

---
+++ app/src/main/java/com/example/android/pictureviewer/AnotherActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.pictureviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnotherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
